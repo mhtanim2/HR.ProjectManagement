67e0dcc947287122ca78c08d52fcc41d03fea219
mhtanim2 <mhtanim2@gmail.com>
2025-12-27 07:58:45 +0600

    Added docker compose file for containerization, both database and api

--- HR.ProjectManagement/Program.cs
+++ HR.ProjectManagement/Program.cs
@@ -46,7 +46,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI(c =>
     {
         c.SwaggerEndpoint("/swagger/v1/swagger.json", "HR Project Management API v1");
-        c.RoutePrefix = string.Empty; // Serve Swagger UI at root
+        c.RoutePrefix = string.Empty; 
         c.ConfigObject.AdditionalItems.Add("persistAuthorization", "true");
     });
 }
